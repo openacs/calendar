6da0d1f9c48ded9405084fd82b43f818e0d71efa
Tom Jackson <tom@zmbh.com>
2002-10-06 16:59:04 +0000

    -fixed create calendar if user does not have personal private calendar -added query file for above query -fixed redirect user to login if necessary

--- www/index.tcl
+++ www/index.tcl
@@ -14,11 +14,17 @@ ad_page_contract {
 set package_id [ad_conn package_id]
 set user_id [ad_conn user_id]
 
-# If there are no calendars at all, we should create a personal one
-set calendar_list [calendar::adjust_calendar_list -calendar_list {} -package_id $package_id -user_id $user_id]
+if {!$user_id} {
+    # user isn't logged in 
+    ad_redirect_for_registration
+    ad_script_abort
+}
 
-# If no calendars, we need at least a personal one!
-if {[llength $calendar_list] == 0} {
+# Check for a personal, private calendar, create if missing
+# FIXME BUG: this should be moved to new pages to allow users
+# to create/delete/modify/admin their calendars. 
+
+if {![db_string private_calendar_count_qry {}]} {
     # Create a personal calendar for the user
     calendar::new -owner_id $user_id -private_p "t" -calendar_name "Personal" -package_id $package_id
 }

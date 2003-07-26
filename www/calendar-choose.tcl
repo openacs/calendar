3f35da0c2ef62d794fee9d37c27a141c9a2c230f
Dirk Gomez <openacs@dirkgomez.de>
2003-07-26 20:54:17 +0000

    moved to using ad_form

--- www/calendar-choose.tcl
+++ www/calendar-choose.tcl
@@ -14,39 +14,22 @@ ad_page_contract {
     {end_time ""}
 }
 
+set user_id [ad_maybe_redirect_for_registration]
+
 set package_id [ad_conn package_id]
 
 set date [calendar::adjust_date -date $date -julian_date $julian_date]
 
 set calendar_list [calendar::calendar_list]
 
-set calendar_id [lindex $calendar_list 0]
-
-set cals_calendar_list [list]
-for { set i 0 } { $i < [llength $calendar_list] } { incr i } {
-    db_1row select_name "select calendar_name from calendars where calendar_id=[lindex $calendar_list $i]"
-    lappend cals_calendar_list [list $calendar_name [lindex $calendar_list $i]]
-}
-
-# Create the form
-form create cals
-
-element create cals return_url \
-    -label "return_url" \
-    -datatype text -widget hidden -value $return_url
-
-element create cals calendar_id \
-    -label "[_ calendar.Calendar]" -datatype text -widget select \
-    -options $cals_calendar_list
-
-# Process the form
-if {[form is_valid cals]} {
-    template::form get_values cals return_url calendar_id
-
-    ad_returnredirect "$return_url&calendar_id=$calendar_id"
+ad_form -name cals -export {return_url} -form {
+    {calendar_id:integer(select)    {label "[_ calendar.Calendar]"}
+                                           {options $calendar_list}
+        {value {[lindex [lindex $calendar_list 0] 1]}}
+    }
     
-    # Stop here
+} -on_submit {
+    ad_returnredirect "$return_url&calendar_id=$calendar_id"
     ad_script_abort
 }
-
 ad_return_template

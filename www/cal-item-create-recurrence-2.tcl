2b86eb0286d5a2bdb9a410a5dac6f678103e9ac1
Dirk Gomez <openacs@dirkgomez.de>
2004-02-19 15:03:43 +0000

    Typo fix

--- www/cal-item-create-recurrence-2.tcl
+++ www/cal-item-create-recurrence-2.tcl
@@ -24,15 +24,20 @@ ad_require_permission $cal_item_id cal_item_write
 # Get basic information about the event. We need the start date
 calendar::item::get -cal_item_id $cal_item_id -array cal_item
 
-set end_date [calendar_make_datetime [array get recur_until]]
 set start_date $cal_item(start_date)
+set end_date [calendar::make_datetime [array get recur_until]]
 
-if {![calendar::item::dates_valid_p -start_date $$cal_item(start_date) -end_date $end_date]} {
+if {![calendar::item::dates_valid_p -start_date $start_date -end_date $end_date]} {
     ad_return_complaint 1 [_ calendar.start_date_before_end_date]
     ad_script_abort
 }
 
 # Set up the recurrence
-calendar::item::add_recurrence -cal_item_id $cal_item_id -interval_type $interval_type -every_n $every_n -days_of_week $days_of_week -recur_until [calendar_make_datetime [array get recur_until]]
+calendar::item::add_recurrence \
+    -cal_item_id $cal_item_id \
+    -interval_type $interval_type \
+    -every_n $every_n \
+    -days_of_week $days_of_week \
+    -recur_until [calendar_make_datetime [array get recur_until]]
 
 ad_returnredirect $return_url

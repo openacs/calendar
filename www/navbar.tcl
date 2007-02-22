6c67e3992f8369810e7d49b6974426dc4a115c31
Cesar Hernandez <cesarhj@galileo.edu>
2007-02-22 22:19:02 +0000

    removing from element list the view calendar, since is unnecessary because the view is the same as month.

--- www/navbar.tcl
+++ www/navbar.tcl
@@ -58,15 +58,6 @@ multirow append views \
     ""
 
 multirow append views \
-    " Calendar" \
-    "calendar" \
-    "[export_vars -base $base_url {date {view month}}]${page_num}\#calendar" \
-    "/resources/calendar/images/calendar-icon.gif" \
-    "&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;" \
-    f \
-    ""
-
-multirow append views \
     " List" \
     "list" \
     "[export_vars -base $base_url {date {view list}}]${page_num}${url_stub_period_days}\#calendar" \

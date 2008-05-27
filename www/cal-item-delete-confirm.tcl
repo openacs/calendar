fd3d06e7b590dfd863e23c6aa8b2cd59df6416d2
Emmanuelle Raffenne <eraffenne@gmail.com>
2008-05-27 15:44:52 +0000

    Replaced table with div. HTML strict cleanup.

--- www/cal-item-delete-confirm.tcl
+++ www/cal-item-delete-confirm.tcl
@@ -26,4 +26,15 @@ set date $cal_item(start_date)
 template::head::add_css -href "/resources/calendar/calendar.css" -media all
 template::head::add_css -alternate -href "/resources/calendar/calendar-hc.css" -title "highContrast"
 
+
+set view_url [export_vars -base "view" {{view day} {date $cal_item(start_date)}}]
+
+if {  $cal_item(recurrence_id) ne "" } {
+    set delete_one [export_vars -base "cal-item-delete" {cal_item_id {confirm_p 1}}]
+    set delete_all [export_vars -base "cal-item-delete-all-occurrences" {{recurrence_id $cal_item(recurrence_id)}}]
+} else {
+    set delete_confirm [export_vars -base "cal-item-delete" {cal_item_id {confirm_p 1}}]
+    set delete_cancel [export_vars -base "cal-item-view" {cal_item_id}]
+}
+
 ad_return_template

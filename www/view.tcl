75e59adf58d22e1622b87d231f673b46626f4b16
Don Baccus <dhogaza@pacifier.com>
2008-09-12 16:04:48 +0000

    Don't encode return_url ...

--- www/view.tcl
+++ www/view.tcl
@@ -33,7 +33,7 @@ if {$export == "print"} {
     set view "list"
 }
 
-set return_url [ad_urlencode [ad_return_url]]
+set return_url [ad_return_url]
 set add_item_url [export_vars -base "cal-item-new" {{return_url [ad_return_url]} view date}]
 
 set admin_p [permission::permission_p -object_id $package_id -privilege calendar_admin]

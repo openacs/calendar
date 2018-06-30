a0cd52d25b85bdae87cf3c47c636f5227c4a9457
Hector Romojaro <hector.romojaro@gmail.com>
2018-06-30 21:08:42 +0000

    Fix proc and contract doc elements, so they are properly parsed by apidoc::api_*_documentation.
        - Add mising @param
        - Add missing @author
        - @creation_date -> @creation-date
        - @creation-user -> @author
        - Fix typos

--- www/cal-options.tcl
+++ www/cal-options.tcl
@@ -1,9 +1,9 @@
 ad_page_contract {
-    
+
     List all the calendars for which the user has the read privilege.
-    
+
     @author Dirk Gomez (openacs@dirkgomez.de)
-    @authorr Gary Jin (gjin@arsdigita.com)
+    @author Gary Jin (gjin@arsdigita.com)
     @author Ben Adida (ben@openforce.net)
     @creation-date Dec 14, 2000, May 29th, 2002
     @cvs-id $Id$

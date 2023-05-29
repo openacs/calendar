1fee9ad89e7d8c6e5432c71d226e7288c37acca1
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2023-05-29 09:45:34 +0000

    Fix for define_function_args for cal_item__new
    - Define always SQL function with the longest argument list
    - Bumped version number to 2.10.1d7

---
+++ sql/postgresql/upgrade/upgrade-2.10.1d6-2.10.1d7.sql
@@ -0,0 +1,2 @@
+
+select define_function_args('cal_item__new','cal_item_id;null,on_which_calendar;null,name,description,html_p;null,status_summary;null,timespan_id;null,activity_id;null,recurrence_id;null,object_type;"cal_item",context_id;null,creation_date;now(),creation_user;null,creation_ip;null,package_id;null,location;null,related_link_url;null,related_link_text;null,redirect_to_rel_link_p;null');
